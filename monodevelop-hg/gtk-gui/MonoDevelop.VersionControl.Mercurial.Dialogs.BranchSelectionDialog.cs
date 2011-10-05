c3b546dc65cbb057dee0bd065386f8484ac429eb
Levi Bard <142250+Tak@users.noreply.github.com>
2011-10-05 13:40:09 +0200

    Reorganize with Transmission subrepo.

--- monodevelop-hg/gtk-gui/MonoDevelop.VersionControl.Mercurial.Dialogs.BranchSelectionDialog.cs
+++ monodevelop-hg/gtk-gui/MonoDevelop.VersionControl.Mercurial.Dialogs.BranchSelectionDialog.cs
@@ -14,7 +14,7 @@ namespace MonoDevelop.VersionControl.Mercurial.Dialogs
 		private global::Gtk.CheckButton omitCB;
 		private global::Gtk.Button buttonCancel;
 		private global::Gtk.Button buttonOk;
-
+		
 		protected virtual void Build ()
 		{
 			global::Stetic.Gui.Initialize (this);
--- monodevelop-hg/gtk-gui/MonoDevelop.VersionControl.Mercurial.Dialogs.PasswordPromptDialog.cs
+++ monodevelop-hg/gtk-gui/MonoDevelop.VersionControl.Mercurial.Dialogs.PasswordPromptDialog.cs
@@ -9,7 +9,7 @@ namespace MonoDevelop.VersionControl.Mercurial.Dialogs
 		private global::Gtk.Entry passwordEntry;
 		private global::Gtk.Button buttonCancel;
 		private global::Gtk.Button buttonOk;
-
+		
 		protected virtual void Build ()
 		{
 			global::Stetic.Gui.Initialize (this);
